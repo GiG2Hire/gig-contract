ff358a370cc0cfd99aadc4240b258eeda52eca51
Andriy Shvets <103654086+alhonaut@users.noreply.github.com>
2024-09-13 03:58:01 +0300

    Delete unused console import

--- test/PaymentTest.t.sol
+++ test/PaymentTest.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.19;
 
-import {Test, console} from "forge-std/Test.sol";
+import {Test} from "forge-std/Test.sol";
 import {USDCPayment} from "../src/PaymentV1.sol";
 import {IUSDC} from "../src/interfaces/IUSDC.sol";
 import {IERC20} from "@chainlink/contracts-ccip/src/v0.8/vendor/openzeppelin-solidity/v4.8.0/contracts/token/ERC20/IERC20.sol";
